de9f2b238e4b95b9036d9661fc3150d603338097
abdullah-tahir16 <aby217@gmail.com>
2020-08-12 22:12:37 +0500

    updated

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const  Navbar =({totalCounters}) => {
+    return ( 
+
+        <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+       <span className="badge badge-pill badge-secondary">{totalCounters}</span>
+        </nav>
+
+
+     );
+
+};
+ 
+export default Navbar;
